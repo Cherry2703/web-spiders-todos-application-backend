cbf89a06c4fb86ead6b4e22ffea8978969882775
Cherry2703 <ramcharanamr2408@gmail.com>
2024-12-07 12:56:34 +0530

    made changes for bcryptjs

--- server.js
+++ server.js
@@ -305,7 +305,7 @@
 
 
 const express = require("express");
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const { v4: uuidv4 } = require("uuid");
 const { MongoClient, ObjectId } = require("mongodb");
